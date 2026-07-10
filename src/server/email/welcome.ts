a732f428f6a40dcf51eae7d86dc18f68abcd76a5
DenisBilurka23 <Denisbilurka23@gmail.com>
2026-07-10 17:20:37 +0300

    Add customer email notifications across order lifecycle stages

--- src/server/email/welcome.ts
+++ src/server/email/welcome.ts
@@ -31,7 +31,6 @@ const buildWelcomeBodyHtml = (
                     ${buildParagraphHtml(strings.para1, 14)}
                     ${buildParagraphHtml(strings.para2, 28)}
                     ${buildButtonHtml(strings.buttonLabel, shopUrl)}
-                    <div style="height:24px;line-height:24px;font-size:0;">&nbsp;</div>
                   </td>
                 </tr>`;
 
